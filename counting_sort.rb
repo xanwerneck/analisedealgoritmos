20b9dc7bd79c48be980cbb5f69dbd051a1cc26fc
Hugo Roque <hugolnx@gmail.com>
2015-03-31 19:33:27 -0300

    only test counting sort if test parameter were passed

--- counting_sort.rb
+++ counting_sort.rb
@@ -11,17 +11,19 @@ def counting_sort(a, min: 0, max:nil)
   sorted
 end
 
-TEST_CASES = [
-  [[3, 1, 2], [1, 2, 3]],
-  [[9, 8, 2,  3, 1, 2], [1, 2, 2, 3, 8, 9]],
-  [[9, 8, 7, 6, 5, 5, 5, 4, 3], [3, 4, 5, 5, 5, 6, 7, 8, 9]]
-]
+if ARGV[0] == "test"
+  TEST_CASES = [
+    [[3, 1, 2], [1, 2, 3]],
+    [[9, 8, 2,  3, 1, 2], [1, 2, 2, 3, 8, 9]],
+    [[9, 8, 7, 6, 5, 5, 5, 4, 3], [3, 4, 5, 5, 5, 6, 7, 8, 9]]
+  ]
 
-failed = TEST_CASES.find{|(array, expected)| counting_sort(array.map{|x|x+900}, min: 900, max: 1000) != expected.map{|x|x+900}}
-if failed.nil?
-  puts "Os testes passaram"
-else
-  array = failed[0]
-  expected = failed[1]
-  puts "O caso #{array.inspect} falhou, retornou #{bigger_sum(array).inspect} mas deveria ser #{expected.inspect}"
+  failed = TEST_CASES.find{|(array, expected)| counting_sort(array.map{|x|x+900}, min: 900, max: 1000) != expected.map{|x|x+900}}
+  if failed.nil?
+    puts "Os testes passaram"
+  else
+    array = failed[0]
+    expected = failed[1]
+    puts "O caso #{array.inspect} falhou, retornou #{bigger_sum(array).inspect} mas deveria ser #{expected.inspect}"
+  end
 end
