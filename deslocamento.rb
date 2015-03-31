9819450bac5c88254a9020046568713d3fbf4cc0
Hugo Roque <hugolnx@gmail.com>
2015-03-31 18:45:40 -0300

    deslocamento em O(n+m)

--- deslocamento.rb
+++ deslocamento.rb
@@ -1,18 +1,33 @@
+BASE = 40
+
+class Array
+  def to_arithmetic_number(base)
+    b = base**self.size
+    self.inject(0){|result, nxt| result + nxt*(b/=base) }
+  end
+
+  def arithmetic_each(size, base, &block)
+    number = self[0...size].to_arithmetic_number(base)
+    yield number, 0
+    base_mult = base**(size-1)
+    self[size..-1].each_with_index do |nxt, i|
+      number = (number - (number/base_mult)*base_mult)*base + nxt
+      yield number, i+1
+    end
+  end
+end
+
 def matching(p, t)
   m = p.size
   n = t.size
+  tnumber = t.to_arithmetic_number(BASE)
   valids = []
-  for i in 0..(m-n)
-    is_valid = true
-    for j in 0...n
-      if p[i+j] != t[j]
-        is_valid = false
-        break
-      end
+  p.arithmetic_each(n, BASE) do |pnumber, start_i|
+    if pnumber == tnumber
+      valids << start_i
     end
-    valids << i if is_valid
   end
-  valids
+  return valids
 end
 
 
@@ -21,7 +36,8 @@ TEST_CASES = [
   [[1,2,3,4,5,1,2], [1,2], [0,5]],
   [[1,2,3,4,5,1,2], [1,2,3], [0]],
   [[1,2,3,4,5,1,2], [1,2,3,9], []],
-  [[1,2,3,4,5,1,2,1], [1], [0,5,7]]
+  [[1,2,3,4,5,1,2,1], [1], [0,5,7]],
+  [[14,29,30,41,15,14,29,30], [14,29,30], [0,5]]
 ]
 
 failing_case = TEST_CASES.find{|(p, t, expected)| matching(p,t) != expected}
