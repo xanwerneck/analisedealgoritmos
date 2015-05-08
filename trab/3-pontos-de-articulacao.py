b40eb7e6ee4ce7c19908ecf506b5080d3afba2ea
Hugo Roque <hugolnx@gmail.com>
2015-05-08 01:21:06 -0300

    tarefa 3, marcando pre/pos

--- trab/3-pontos-de-articulacao.py
+++ trab/3-pontos-de-articulacao.py
@@ -1,21 +1,46 @@
 from graph_base import adjacents, nodes
 
 def articulation_points():
+  visited = {}
+  pre = {}
+  pos = {}
+  in_cicle = {}
   for state in nodes():
     if visited.get(state):
       continue
     print "componente"
-    visited = dict([(state,True)])
-    pre = {}
-    pos = {}
+    next_mark = 0
 
-    stack = [state]
+    stack = [[None, state]]
     while len(stack) != 0:
-      node = stack.pop()
+      block = stack[-1]
+      while block and len(block) == 1:
+        pos[block[0]] = next_mark
+        next_mark += 1
+        stack.pop()
+        block = stack[-1] if len(stack) > 0 else None
+
+      if block is None: break
+
+      node = block.pop()
+      if visited.get(node): continue
+
+      visited[node] = True
+      pre[node] = next_mark
+      next_mark += 1
+      block = [node]
       for adjacent in adjacents(node):
-        if visited.get(adjacent):
-          continue
-        visited[adjacent] = True
-        stack.append(adjacent)
+        block.append(adjacent)
+      stack.append(block)
+  print pre
+  print pos
 
 print articulation_points()
+#       
+#       .
+#     . . . .
+#     .     . .
+#   . .     .
+#     . . . .
+#         .
+#
