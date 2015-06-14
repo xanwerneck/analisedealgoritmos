da4d9c3b01c95c8a1e950f053cb1a9dee5ceb6bf
Alexandre Werneck <xanwerneck@gmail.com>
2015-06-14 01:01:48 -0300

    fix bugs on job 1

--- trab/deploy/1-componentes_conexas.py
+++ trab/deploy/1-componentes_conexas.py
@@ -2,8 +2,9 @@ import time
 from graph_base import adjacents, nodes
 
 def dfs(graph):
+
   visited = {} # armazena todos os nos visitados
-  trees   = [] # array com as arvores formadas na visita ao no
+  trees   = [] # array com as componentes + arestas
   for node in graph:
     if visited.get(node) == None:
       tree = search(graph, node, visited)
@@ -12,7 +13,12 @@ def dfs(graph):
 
 
 def search(graph, nod, visited):
+  retorno  = []     # retorna o caminho + arestas da componente
   tree_way = {}     # contem o caminho pela componente conexa
+  edges_c  = []     # mantem as arestas
+  retorno.append(tree_way)
+  retorno.append(edges_c)
+
   stack    = []     # pilha para eliminar as recursoes
   stack.append(nod) # inicia a pilha com o no de entrada
 
@@ -21,10 +27,12 @@ def search(graph, nod, visited):
     for node_adj in adjacents(node):
       if not tree_way.get(node_adj) and not visited.get(node_adj):
         stack.append(node_adj)
+        edge = [node, node_adj]
+        edges_c.append(edge)
     tree_way[node] = 1
     visited[node]  = 1
 
-  return tree_way
+  return retorno
 
 
 ini = time.time()
@@ -36,5 +44,5 @@ print "Numero de comp. conexas: ", len(visit)
 print "---------------------------------"
 for x in xrange(0, len(visit)):
   print "---------COMPONENTE-", x + 1
-  print "-Qtde vertices-", len(visit[x])
-  print "-Qtde arestas- ", len(visit[x]) - 1
+  print "-Qtde vertices-", len(visit[x][0])
+  print "-Qtde arestas- ", len(visit[x][1])
